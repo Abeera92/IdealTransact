da54981804bb3190143793d8f0963c03bca1e236
Dark Pheonix <hashimshub81@gmail.com>
2025-11-28 11:02:20 +0500

    about bg updated

--- src/Components/AboutHero/index.jsx
+++ src/Components/AboutHero/index.jsx
@@ -162,7 +162,7 @@ export default function AboutHero() {
       </div>
 
       {/* Scroll Indicator */}
-      <motion.div
+      {/* <motion.div
         className="absolute bottom-8 left-1/2 transform -translate-x-1/2 text-white z-20"
         animate={{ y: [0, 10, 0] }}
         transition={{ duration: 2, repeat: Infinity }}
@@ -173,7 +173,7 @@ export default function AboutHero() {
             <div className="w-1 h-3 bg-white/50 rounded-full mt-2"></div>
           </div>
         </div>
-      </motion.div>
+      </motion.div> */}
     </section>
   );
 }
