19c8ac645b7f5c454ff911fef2f4752f1588091d
Dark Pheonix <hashimshub81@gmail.com>
2026-01-05 10:44:26 +0500

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ import EcommercePayments from "./pages/Services/EcomPayment";
 import MerchantAccountSetup from "./pages/Services/MerchentAcc";
 import FraudPrevention from "./pages/Services/FraudPrevention";
 import MerchantSupport from "./pages/Services/Support";
-import FeaturedProjects from "./Components/FeaturedProjects";
+// import FeaturedProjects from "./Components/FeaturedProjects";
 
 function App() {
   return (
@@ -65,7 +65,7 @@ function App() {
             <Route path="/contact" element={<Contact />} />
           </Routes>
 
-          <FeaturedProjects/>
+          {/* <FeaturedProjects/> */}
         </main>
 
         <Footer />
